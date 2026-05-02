fb3fd27804278730eaa868ac26bd3526ac58abdc
ekta-developer <eekta.srivastava2000@gmail.com>
2026-05-02 16:25:53 +0530

    fixed route fixed
    fixed route fixed

--- routes/businessHackRoutes.js
+++ routes/businessHackRoutes.js
@@ -6,21 +6,15 @@ import {
   getBusinessHackById,
   updateBusinessHack,
   deleteBusinessHack,
-} from "../controller/businessHacksVideoController.js";
+} from "../controller/businessHackController.js";
 
 import { verifyToken } from "../middleware/AuthMiddleware.js";
-
-import { uploadBusinessHack } from "../middleware/uploadBusinessHacksMedia.js";
 
 const router = express.Router();
 
 router.post(
   "/create",
   verifyToken,
-  uploadBusinessHack.fields([
-    { name: "thumbnail", maxCount: 1 },
-    { name: "video", maxCount: 1 },
-  ]),
   createBusinessHack
 );
 
@@ -31,10 +25,6 @@ router.get("/:id", verifyToken, getBusinessHackById);
 router.put(
   "/:id",
   verifyToken,
-  uploadBusinessHack.fields([
-    { name: "thumbnail", maxCount: 1 },
-    { name: "video", maxCount: 1 },
-  ]),
   updateBusinessHack
 );
 
